ae446d1c21c93b3a23d7f03d0a33d5ada9c49f61
NMTuan <nmtuan@qq.com>
2024-03-29 11:07:21 +0800

    feat: 增加一个文字展示区域，可以填写一句提醒自己的文字。

--- app.vue
+++ app.vue
@@ -2,14 +2,14 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2024-02-18 11:35:10
- * @LastEditTime: 2024-02-27 17:47:01
+ * @LastEditTime: 2024-03-29 10:39:20
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \timeNow\app.vue
 -->
 <template>
     <div class="group w-full h-screen overflow-hidden flex flex-col items-center justify-between">
-        <Header class="opacity-0 transition-all group-hover:opacity-100"></Header>
+        <Header> </Header>
         <div class="bg-white/5 w-full flex flex-col items-center justify-center py-[4vw]">
             <div class="select-none">
                 <ClockWeek></ClockWeek>
@@ -17,7 +17,7 @@
                 <ClockDate></ClockDate>
             </div>
         </div>
-        <div class="w-full px-[4px] mt-[26px]">
+        <div class="w-full px-[4px] mt-[26px] flex-shrink-0">
             <div v-if="settingStore.showTimeLine" class="px-[4px]">
                 <TimeLine></TimeLine>
             </div>
--- stores/setting.ts
+++ stores/setting.ts
@@ -2,41 +2,47 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2024-02-21 21:47:50
- * @LastEditTime: 2024-02-27 18:17:24
+ * @LastEditTime: 2024-03-29 10:53:42
  * @LastEditors: NMTuan
  * @Description:
  * @FilePath: \timeNow\stores\setting.ts
  */
 import { defineStore } from 'pinia'
 
+const cookieMaxAge: number = 31536000
+
 export const useSettingStore = defineStore('setting', () => {
     const hour24 = useCookie('hour24', {
-        maxAge: 31536000,
+        maxAge: cookieMaxAge,
         default: () => true
     }) // 24小时制
     const showSecond = useCookie('showSecond', {
-        maxAge: 31536000,
+        maxAge: cookieMaxAge,
         default: () => true
     }) // 显示秒数
     const flicker1 = useCookie('flicker1', {
-        maxAge: 31536000,
+        maxAge: cookieMaxAge,
         default: () => false
     }) // 闪烁冒号 时分
     const flicker2 = useCookie('flicker2', {
-        maxAge: 31536000,
+        maxAge: cookieMaxAge,
         default: () => false
     }) // 闪烁冒号 分秒
     const showMonthLine = useCookie('showMonthLine', {
-        maxAge: 31536000,
+        maxAge: cookieMaxAge,
         default: () => true
     })
     const showDayLine = useCookie('showDayLine', {
-        maxAge: 31536000,
+        maxAge: cookieMaxAge,
         default: () => true
     })
     const showTimeLine = useCookie('showTimeLine', {
-        maxAge: 31536000,
+        maxAge: cookieMaxAge,
         default: () => true
+    })
+    const motto = useCookie('motto', {
+        maxAge: cookieMaxAge,
+        default: () => ''
     })
     return {
         hour24,
@@ -45,6 +51,7 @@ export const useSettingStore = defineStore('setting', () => {
         flicker2,
         showMonthLine,
         showDayLine,
-        showTimeLine
+        showTimeLine,
+        motto
     }
 })
