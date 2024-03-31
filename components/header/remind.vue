6cf646b20662498499c23196ff660a9d8909e333
nmtuan <nmtuan@qq.com>
2024-03-31 15:54:03 +0800

    fix: 修复 打赏窗 和 提醒窗 不能自动关闭的问题

--- components/header/remind.vue
+++ components/header/remind.vue
@@ -2,7 +2,7 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2024-03-30 21:17:15
- * @LastEditTime: 2024-03-31 11:50:04
+ * @LastEditTime: 2024-03-31 15:51:22
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \timeNow\components\header\remind.vue
@@ -10,7 +10,7 @@
 <template>
     <ClientOnly>
         <HeaderIcon name="i-ri-notification-2-line" @click="showSlide = true"></HeaderIcon>
-        <USlideover v-model="showSlide" prevent-close>
+        <USlideover v-model="showSlide">
             <ComCard label="remind" :close="closeSlide">
                 <div v-if="remindStore.list.length === 0" class="flex flex-col items-center">
                     <div class="text-gray-500">
@@ -19,7 +19,7 @@
                 </div>
                 <div v-else>
                     <div v-for="item in remindStore.list">
-                        <UAlert class="mb-4"
+                        <UAlert class="mb-4" :title="item.time"
                             :close-button="{ icon: 'i-heroicons-x-mark-20-solid', color: 'gray', variant: 'link', padded: false }"
                             @close="remindStore.remove(item.id)">
 
